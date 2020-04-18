e33e637451aaa9033292d343f8c454ccb4081151
Luc <luc.rosenzweig@yahoo.com>
2020-04-18 23:44:40 +0100

    Added some code for hub

---
+++ source/common/hub_packet.cpp
@@ -0,0 +1,77 @@
+#include "hub_packet.hpp"
+#include "allocators.hpp"
+
+void serialise_hub_packet_header(
+    hub_packet_header_t *header,
+    serialiser_t *serialiser) {
+    serialiser->serialise_uint32(header->type);
+}
+
+void deserialise_hub_packet_header(
+    hub_packet_header_t *header,
+    serialiser_t *serialiser) {
+    header->type = (hub_packet_type_t)serialiser->deserialise_uint32();
+}
+
+void serialise_hub_query_server_register(
+    hub_query_server_register_t *query,
+    serialiser_t *serialiser) {
+    serialiser->serialise_string(query->server_name);
+    serialiser->serialise_uint32(query->max_clients);
+    serialiser->serialise_uint32(query->client_count);
+}
+
+void deserialise_hub_query_server_register(
+    hub_query_server_register_t *query,
+    serialiser_t *serialiser) {
+    query->server_name = serialiser->deserialise_string();
+    query->max_clients = serialiser->deserialise_uint32();
+    query->client_count = serialiser->deserialise_uint32();
+}
+
+void serialise_hub_query_client_register(
+    hub_query_client_register_t *query,
+    serialiser_t *serialiser) {
+    serialiser->serialise_string(query->client_name);
+}
+
+void deserialise_hub_query_client_register(
+    hub_query_client_register_t *query,
+    serialiser_t *serialiser) {
+    query->client_name- serialiser->deserialise_string();
+}
+
+void serialise_hub_server_info(
+    hub_server_info_t *info,
+    serialiser_t *serialiser) {
+    serialiser->serialise_string(info->server_name);
+    serialiser->serialise_uint32(info->max_clients);
+    serialiser->serialise_uint32(info->client_count);
+}
+
+void deserialise_hub_server_info(
+    hub_server_info_t *info,
+    serialiser_t *serialiser) {
+    info->server_name = serialiser->deserialise_string();
+    info->max_clients = serialiser->deserialise_uint32();
+    info->client_count = serialiser->deserialise_uint32();
+}
+
+void serialise_hub_response_available_servers(
+    hub_response_available_servers_t *response,
+    serialiser_t *serialiser) {
+    serialiser->serialise_uint32(response->server_count);
+    for (uint32_t i = 0; i < response->server_count; ++i) {
+        serialise_hub_server_info(&response->servers[i], serialiser);
+    }
+}
+
+void deserialise_hub_response_available_servers(
+    hub_response_available_servers_t *response,
+    serialiser_t *serialiser) {
+    response->server_count = serialiser->deserialise_uint32();
+    response->servers = LN_MALLOC(hub_server_info_t, response->server_count);
+    for (uint32_t i = 0; i < response->server_count; ++i) {
+        deserialise_hub_server_info(&response->servers[i], serialiser);
+    }
+}
---
+++ source/common/hub_packet.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "tools.hpp"
+#include "serialiser.hpp"
+
+enum hub_packet_type_t : uint32_t {
+    HPT_QUERY_SERVER_REGISTER,
+    HPT_QUERY_CLIENT_REGISTER,
+    
+    HPT_QUERY_AVAILABLE_SERVERS,
+    HPT_RESPONSE_AVAILABLE_SERVERS,
+
+    HPT_INFORM_CLIENT_COUNT,
+};
+
+struct hub_packet_header_t {
+    hub_packet_type_t type;
+};
+
+void serialise_hub_packet_header(
+    hub_packet_header_t *header,
+    serialiser_t *serialiser);
+
+void deserialise_hub_packet_header(
+    hub_packet_header_t *header,
+    serialiser_t *serialiser);
+
+struct hub_query_server_register_t {
+    const char *server_name;
+
+    uint32_t max_clients;
+    uint32_t client_count;
+};
+
+void serialise_hub_query_server_register(
+    hub_query_server_register_t *query,
+    serialiser_t *serialiser);
+
+void serialise_hub_query_server_register(
+    hub_query_server_register_t *query,
+    serialiser_t *serialiser);
+
+struct hub_query_client_register_t {
+    const char *client_name;
+};
+
+void serialise_hub_query_client_register(
+    hub_query_client_register_t *query,
+    serialiser_t *serialiser);
+
+void serialise_hub_query_client_register(
+    hub_query_client_register_t *query,
+    serialiser_t *serialiser);
+
+struct hub_query_available_servers_t {
+    // In future, have filters
+};
+
+struct hub_server_info_t {
+    const char *server_name;
+    uint32_t max_clients;
+    uint32_t client_count;
+};
+
+void serialise_hub_server_info(
+    hub_server_info_t *info,
+    serialiser_t *serialiser);
+
+void serialise_hub_server_info(
+    hub_server_info_t *info,
+    serialiser_t *serialiser);
+
+struct hub_response_available_servers_t {
+    uint32_t server_count;
+    hub_server_info_t *servers;
+};
+
+void serialise_hub_response_available_servers(
+    hub_response_available_servers_t *response,
+    serialiser_t *serialiser);
+
+void serialise_hub_response_available_servers(
+    hub_response_available_servers_t *response,
+    serialiser_t *serialiser);
